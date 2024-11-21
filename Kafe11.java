d5642fc8e40f0b3d2bace6432d44c5dc77eeb2eb
haf5th <hafiflearning@gmail.com>
2024-11-21 23:19:00 +0700

    Commit 2

--- Kafe11.java
+++ Kafe11.java
@@ -1,5 +1,6 @@
 public class Kafe11 {
-    public static void Menu(){
+    public static void main(String[] args) {
+        
         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam - Rp 15,000");
         System.out.println("2. Cappuccino - Rp 20,000");
@@ -10,7 +11,5 @@ public class Kafe11 {
         System.out.println("===========================");
         System.out.println("Silahkan pilih menu yang Anda inginkan.");
     }
-    public static void main(String[] args) {
-        Menu();
-    }
+    
 }
